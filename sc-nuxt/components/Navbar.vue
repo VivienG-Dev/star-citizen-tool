30693a8931bd3b074d4737b519bc65cf34cbca1d
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-01-04 10:04:10 +0100

    Update Navbar link to use production URL

--- sc-nuxt/components/Navbar.vue
+++ sc-nuxt/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="flex gap-4 justify-between bg-customPrimary p-3 mb-10 clip-path text-gray-100">
-      <nuxt-link to="http://localhost:3000/" class="font-bold">Star Citizen Tool</nuxt-link>
+      <nuxt-link to="https://www.star-citizen-tool.vivieng.com/" class="font-bold">Star Citizen Tool</nuxt-link>
       <div class="flex gap-4">
         <nuxt-link to="/api" class="hover:text-customSecondary">API</nuxt-link>
       </div>
